7d1a85ad1aeab8162ecdb0a15b4b01154dbd7975
Darius Bolos <bolosdarius@gmail.com>
2023-11-30 06:15:14 +0200

    fix: every piece moves as it should now

--- components/Game.ts
+++ components/Game.ts
@@ -1,9 +1,16 @@
+import { Pawn } from './Pawn';
+import { King } from './King';
+import { Queen } from './Queen';
+import { Bishop } from './Bishop';
+import { Knight } from './Knight';
+import { Rook } from './Rook';
+
 export class Game {
-    clearPage(container:HTMLElement) {
+    clearPage(container: HTMLElement) {
         container.style.display = 'none';
     }
 
-    showBoard(container:HTMLElement) {
+    showBoard(container: HTMLElement) {
         container.style.display = 'flex';
         container.style.flexWrap = 'wrap';
     }
@@ -11,5 +18,18 @@ export class Game {
     changePlayer(currentPlayer: string){
         currentPlayer === 'white'? currentPlayer = 'black' : currentPlayer = 'white'
         return currentPlayer
+    }
+
+    createPiece(pieceType: string){
+        const piece = {
+            'pawn': new Pawn(),
+            'king': new King(),
+            'queen': new Queen(),
+            'bishop': new Bishop(),
+            'knight': new Knight(),
+            'rook': new Rook()
+        }
+
+        return piece[pieceType]
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import './style.css'
 import { Game } from '../components/Game.ts'
 import { Board } from '../components/Board.ts'
-import { Rook } from '../components/Rook.ts';
-import { Queen } from '../components/Queen.ts'
+
 
 const game = new Game()
 const board = new Board()
-const rookObj = new Rook()
-const queenObj = new Queen()
 const startContainer = document.querySelector('.start-container') as HTMLElement
 const startButton = document.querySelector('.start-button') as HTMLElement
 const chessBoard = document.querySelector('.chess-board') as HTMLElement
@@ -32,11 +29,13 @@ startButton.addEventListener('click', () => {
     let isBeingDragged : HTMLElement
     let startPosition : number
     let player = 'white'
+    let pieceName : string
 
     chessPieces.forEach(piece =>
         piece.addEventListener('dragstart', (e: Event) => {
             isBeingDragged = board.dragStart(e, player) as HTMLElement
             startPosition = Number(isBeingDragged.parentElement.getAttribute('square-id'))
+            pieceName = isBeingDragged.getAttribute('id')
         })
     )
 
@@ -45,8 +44,8 @@ startButton.addEventListener('click', () => {
         square.addEventListener('drop', (e: Event) => {
             const dropTarget = e.target as HTMLElement
             const dropSquareId = Number(dropTarget.getAttribute('square-id'))
-            console.log(queenObj.move(startPosition, dropSquareId))
-            if(queenObj.move(startPosition, dropSquareId))
+            const pieceObj = game.createPiece(pieceName)
+            if(pieceObj.move(startPosition, dropSquareId))
             {
                 player = board.dragDrop(e, isBeingDragged, player)
             
